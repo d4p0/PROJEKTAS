4c1466fdff4afa43193f1149bd97d331ba14fa21
d4p0 <povilas.daniulaitis@gmail.com>
2020-07-16 13:35:49 +0300

    finishing up

---
+++ PROJEKTAS/js/counter.js
@@ -0,0 +1,18 @@
+const speed = 600;
+
+document.querySelectorAll('.counter').forEach(counter => {
+
+	const counterNumber = +counter.getAttribute('countTo');
+
+	const updateCount = setInterval(() => {
+        
+		const divContent = +counter.innerText;
+		const increaseBy = counterNumber / speed;
+
+		divContent < counterNumber ? 
+		counter.innerHTML = Math.ceil(divContent + increaseBy) :
+		clearInterval(updateCount);
+
+	}, 1);
+
+});
--- PROJEKTAS/js/script.js
+++ PROJEKTAS/js/script.js
@@ -13,3 +13,24 @@ if (slideIndex > slides.length) {slideIndex = 1;}
 slides[slideIndex-1].style.display = "block";
 setTimeout(showSlides, 5000);
 }
+
+
+// COUNTER
+const speed = 200;
+
+document.querySelectorAll('#counter').forEach(counter => {
+
+	const counterNumber = +counter.getAttribute('countTo');
+
+	const updateCount = setInterval(() => {
+        
+		const divContent = +counter.innerText;
+		const increaseBy = counterNumber / speed;
+
+		divContent < counterNumber ? 
+		counter.innerHTML = Math.ceil(divContent + increaseBy) : 
+		clearInterval(updateCount);
+
+	}, 1);
+
+});
